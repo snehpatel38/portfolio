9efdcfa3aa609621242c0fa1781655af9767a58f
Sneh Patel <snehpatel0308@gmail.com>
2024-12-24 01:43:08 +0530

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-sneh-patel/',
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  build: {
+    outDir: 'build', // Ensure the output directory matches your deploy script
   },
 });
